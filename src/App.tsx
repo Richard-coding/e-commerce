2748c6b872529c0708aad9d17a03ff87fa9579c3
Richard-coding <richardcoding@outlook.com>
2026-05-21 13:39:17 -0300

    chore: folders structure and initial routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+
+        <Route path="/*" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
