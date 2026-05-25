c3ccf86c4b0068fa9aa45799809f6b08b88cb817
Richard-coding <richardcoding@outlook.com>
2026-05-25 10:59:01 -0300

    feat: add icons in dropdown and responsive logic

--- src/components/content/Header.tsx
+++ src/components/content/Header.tsx
@@ -2,25 +2,62 @@ import { NavLink } from "react-router-dom";
 
 import Shrimp from "../../assets/icons/shrimp.svg?react";
 import Bar from "../../assets/icons/bar.svg?react";
-import User from "../../assets/icons/User.svg?react";
-import Shopping from "../../assets/icons/Shopping.svg?react";
-import { useState } from "react";
+import User from "../../assets/icons/user.svg?react";
+import Shopping from "../../assets/icons/shopping.svg?react";
+import Exit from "../../assets/icons/exit.svg?react";
+import Food from "../../assets/icons/food.svg?react";
+import ShoppingCart from "../../assets/icons/shopping-cart.svg?react";
+import Privacy from "../../assets/icons/privacy.svg?react";
+import { useEffect, useRef, useState } from "react";
 
 const links = [
-  { label: "Cardápio", to: "/cardapio" },
-  { label: "Pedido", to: "/pedido" },
-  { label: "Carrinho", to: "/carrinho" },
-  { label: "Privacidade", to: "/lgpd" },
+  { label: "Cardápio", to: "/cardapio", icon: Food },
+  { label: "Pedido", to: "/pedido", icon: Shopping },
+  { label: "Carrinho", to: "/carrinho", icon: ShoppingCart },
+  { label: "Privacidade", to: "/lgpd", icon: Privacy },
 ];
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const [mobile, setMobile] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement | null>(null);
+  const mobileRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as Node;
+
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(target)
+      ) {
+        setMenu(false);
+      }
+
+      if (
+        mobileRef.current &&
+        !mobileRef.current.contains(target)
+      ) {
+        setMobile(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className="w-full shadow-sm top-0 z-50">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
         <div className="flex items-center gap-3">
-          <div className="w-11 h-11 rounded-full flex items-center justify-center bg-primary shadow-md">
-            <Shrimp className="w-5 h-5 text-white" />
+          <div className="w-11 h-11 rounded-full flex items-center justify-center bg-primary shadow-md hover:bg-secondary  cursor-pointer">
+            <NavLink to="/home">
+              <Shrimp className="w-5 h-5 text-white" />
+            </NavLink>
           </div>
 
           <div>
@@ -58,24 +95,27 @@ const Header = () => {
                 1
               </span>
             </NavLink>
-
-            <div className="flex items-center text-sm gap-2 text-primary relative">
+            <div
+              ref={menuRef}
+              className="flex items-center text-sm gap-2 text-black relative"
+            >
               <p>Richard</p>
+
               <button
-                className="p-3 rounded-full  hover:bg-primary/10"
-                onClick={() => setIsOpen((prev) => !prev)}
+                className="p-3 rounded-full hover:bg-primary/10 cursor-pointer"
+                onClick={() => setMenu((prev) => !prev)}
               >
-                <User className="w-5 h-5 text-foreground " />
+                <User className="w-5 h-5 text-foreground" />
               </button>
 
-              {isOpen && (
-                <div className="absolute w-40 p-2 bg-white top-12 right-0 shadow-md rounded-xl border border-muted/10">
-                  <ul className="flex flex-col gap-2 py-2">
+              {menu && (
+                <div className="absolute w-40 bg-white top-12 right-0 shadow-md rounded-xl border border-muted/10 p-1">
+                  <ul className="flex flex-col gap-2">
                     <li>
-                      <button className="hover:bg-primary/10 py-1 w-full rounded-xl">Sair</button>
-                    </li>
-                    <li>
-                      <button className="hover:bg-primary/10 py-1 w-full rounded-xl">Sair</button>
+                      <button className="hover:bg-primary/10 p-2 w-full rounded-xl flex gap-4 items-center cursor-pointer text-sm">
+                        <Exit className="w-5 h-5"/>
+                        Sair
+                      </button>
                     </li>
                   </ul>
                 </div>
@@ -84,9 +124,40 @@ const Header = () => {
           </div>
         </div>
 
-        <button className="sm:hidden bg-primary text-white p-3 rounded-full shadow-md transition-all duration-200 hover:bg-secondary hover:scale-105">
-          <Bar className="w-5 h-5" />
-        </button>
+        <div ref={mobileRef} className="relative">
+          <button
+            className="sm:hidden cursor-pointer bg-primary text-white p-3 rounded-full shadow-md transition-all duration-200 hover:bg-secondary "
+            onClick={() => setMobile((prev) => !prev)}
+          >
+            <Bar className="w-5 h-5" />
+          </button>
+
+          {mobile && (
+            <div className="absolute w-40 bg-white top-12 right-0 shadow-md rounded-xl border border-muted/10 p-1 text-sm">
+              <ul className="flex flex-col">
+                <li className="flex flex-col gap-2">
+                  {links.map(({ label, to, icon: Icon }) => (
+                    <NavLink
+                      key={label}
+                      to={to}
+                      className="hover:bg-primary/10 py-2 px-4 w-full rounded-xl flex gap-4 items-center "
+                    >
+                      <Icon className="w-5 h-5" />
+                      {label}
+                    </NavLink>
+                  ))}
+                </li>
+
+                <li>
+                  <button className="hover:bg-primary/10 py-2 p-4 w-full rounded-xl flex gap-4 items-center cursor-pointer">
+                    <Exit className="w-5 h-5" />
+                    Sair
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
